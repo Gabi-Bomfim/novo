8769300cff81f0590edd1db6dec01c25690a346d
ThyalaDias <dias.thyala@gmail.com>
2023-09-26 23:13:48 -0300

    troca de formulario

--- script/pagamento.js
+++ script/pagamento.js
@@ -1,21 +1,16 @@
-// Seleciona os radio buttons
-const radioCartaoCredito = document.getElementById("Cartão de Crédito");
-const radioCartaoDebito = document.getElementById("Cartão de Débito");
-const cartaoForm = document.getElementById("cartaoForm");
+// Obtém referências para os elementos dos formulários
+const pixForm = document.getElementById("pixForm");
+const cartaoCreditoForm = document.getElementById("cartaoCreditoForm");
+const cartaoDebitoForm = document.getElementById("cartaoDebitoForm");
 
-// Adiciona um evento de escuta para os radio buttons
-radioCartaoCredito.addEventListener("change", function () {
-    if (this.checked) {
-        cartaoForm.classList.remove("hidden");
-    } else {
-        cartaoForm.classList.add("hidden");
+// Função para alternar entre os formulários com base no tipo de pagamento selecionado
+function alternarFormulario(tipoPagamento) {
+    pixForm.style.display = tipoPagamento === "pix" ? "block" : "none";
+    cartaoCreditoForm.style.display = tipoPagamento === "cartaoCredito" ? "block" : "none";
+    cartaoDebitoForm.style.display = tipoPagamento === "cartaoDebito" ? "block" : "none";
     }
-});
 
-radioCartaoDebito.addEventListener("change", function () {
-    if (this.checked) {
-        cartaoForm.classList.remove("hidden");
-    } else {
-        cartaoForm.classList.add("hidden");
-    }
-});
+// Adicione um evento de clique aos rótulos dos métodos de pagamento
+document.querySelector("label[for=pix]").addEventListener("click", () => alternarFormulario("pix"));
+document.querySelector("label[for=cartaoCredito]").addEventListener("click", () => alternarFormulario("cartaoCredito"));
+document.querySelector("label[for=cartaoDebito]").addEventListener("click", () => alternarFormulario("cartaoDebito"));
